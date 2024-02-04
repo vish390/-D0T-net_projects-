3f4d4b60ffbc59a4c2d675755a9478ec499e61b0
Vishal Navnath Waghmode <72240870+vish390@users.noreply.github.com>
2024-02-04 15:34:39 +0530

    Add files via upload

---
+++ StudentCurd/src/main/java/com/example/StudentCurd/StudentCurdApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudentCurd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCurdApplication.class, args);
+	}
+
+}
---
+++ StudentCurd/src/test/java/com/example/StudentCurd/StudentCurdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudentCurd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
